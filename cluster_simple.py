1bd6125ab2f797ca92d1c60a12fd6f8c7286876c
Matthew Wardrop <matthew.wardrop@airbnb.com>
2016-03-09 22:26:09 -0800

    Rewrite of the transforms into "transform pipes".

--- cluster_simple.py
+++ cluster_simple.py
@@ -13,126 +13,274 @@ import os, sys, csv
 import numpy as np
 import pandas as pd
 
-class AbstractClusterer(object):
+#from hive_transforms import AbstractClusterer, AbstractAggregator, AbstractCluster, Transformer
 
+# t = Transform().map(IdentityMapper).cluster(NullClusterer).reduce(PrintAggregator).output(PrintOutputter)
+# t.apply(data_iterable)
+
+class Transform(object):
+
+    def __init__(self):
+        self._pipeline = []
+        self._outputters = []
+
+    def __ins_add(self, list, obj, type, args, kwargs):
+        if inspect.isclass(obj):
+            obj = obj(*args, **kwargs)
+        elif inspect.isfunction(obj):
+            if type is not 'output':
+                obj = FunctionWrapperTransformPipe(obj, type)
+            else:
+                obj = FunctionWrapperTransformOutput(obj, type)
+        assert obj.type is None or obj.type == type, "Object %s is not of the right type." % obj
+        list.append(obj)
+        return self
+
+    def input(self, mapper, *args, **kwargs):
+        assert len(self._pipeline) == 0, "Input must be the first transform pipeline to be added."
+        return self.__ins_add(self._pipeline, mapper, 'input', args, kwargs)
+
+    def map(self, mapper, *args, **kwargs):
+        return self.__ins_add(self._pipeline, mapper, 'map', args, kwargs)
+
+    def cluster(self, clusterer, *args, **kwargs):
+        return self.__ins_add(self._pipeline, clusterer, 'cluster', args, kwargs)
+
+    def aggregate(self, reducer, *args, **kwargs):
+        return self.__ins_add(self._pipeline, reducer, 'agg', args, kwargs)
+
+    def tee(self, tee, *args, **kwargs):
+        if not isinstance(tee, TeeTransformPipe):
+            tee = TeeTransformPipe(tee)
+        return self.__ins_add(self._pipeline, tee, 'tee', args, kwargs)
+
+    def output(self, outputter, *args, **kwargs):
+        return self.__ins_add(self._outputters, outputter, 'output', args, kwargs)
+
+    def apply(self, data):
+        assert len(self._outputters) > 0, "This transform does not output anything!"
+
+        r = data
+        for tp in self._pipeline:
+            r = tp.apply(r)
+
+        for output in r:
+            for outputter in self._outputters:
+                outputter.apply(output)
+
+class TransformPipe(object):
     __metaclass__ = ABCMeta
 
-    def __init__(self, **kwargs):
-        self.groups_handled = {}
-        self.init(**kwargs)
-        self.reset()
+    def __init__(self, *args, **kwargs):
+        self.init(*args, **kwargs)
+
+    def __call__(self, *args, **kwargs):
+        for result in self.apply(*args, **kwargs):
+            yield result
 
     @abstractmethod
-    def init(self, **kwargs):
+    def init(self, *args, **kwargs):
         pass
 
     @abstractmethod
-    def reset(self):
-        pass
-
-    def __call__(self, data):
-        return self.cluster(data)
-
-    @abstractmethod
-    def cluster(self, data):
-        pass
-
-    @classmethod
-    def setup_parsers(cls, subparsers):
-        parser = subparsers.add_parser(cls.__shortname__)
-        parser.set_defaults(which=cls.__shortname__)
-        for arg in inspect.getargspec(cls.init).args[1:]:
-            parser.add_argument(cls.__shortname__+"_"+arg, action="store")
-
-class AbstractCluster(object):
-
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
-    def merge(self, other):
-        pass
-
-    @abstractmethod
-    def should_include(self, datum):
-        pass
-
-    @abstractmethod
-    def add(self, *ids):
+    def apply(self, data):
         pass
 
     @abstractproperty
-    def props(self):
+    def type(self):
         pass
 
-class AbstractAggregator(object):
+class FunctionWrapperTransformPipe(TransformPipe):
 
-    __metaclass__ = ABCMeta
+    def init(self, function, type=None):
+        assert callable(function), "Wrapped objects must be callable."
+        self.__function = function
+        self._type = type
 
-    def __init__(self, fields):
+    def apply(self, data):
+        for result in self.__function(data):
+            yield result
+
+    @property
+    def type(self):
+        return self._type
+
+class FunctionWrapperTransformOutput(TransformPipe):
+
+    def init(self, function, type=None):
+        assert callable(function), "Wrapped objects must be callable."
+        self.__function = function
+        self._type = type
+
+    def apply(self, data):
+        return self.__function(data)
+
+    @property
+    def type(self):
+        return self._type
+
+class TeeTransformPipe(TransformPipe):
+
+    def init(self, outputter):
+        self.is_function = inspect.isfunction(outputter)
+        if not self.is_function:
+            assert outputter.type == 'output'
+        self.__outputter = outputter
+
+    def apply(self, data):
+        for row in data:
+            if self.is_function:
+                self.__outputter(row)
+            else:
+                self.__outputter.apply(row)
+            yield row
+
+    @property
+    def type(self):
+        return 'tee'
+
+class HiveToDictInput(TransformPipe):
+
+    def init(self, fields):
         self.fields = fields
 
-    def __call__(self, *args, **kwargs):
-        return self.aggregate(*args, **kwargs)
+    def apply(self, data):
+        return csv.DictReader(data, delimiter='\t', doublequote=False, fieldnames=self.fields)
 
-    @abstractmethod
-    def aggregate(self, data, cluster, results=None):
-        pass
+    @property
+    def type(self):
+        return 'input'
 
-class PrintAggregator(AbstractAggregator):
+class DictToHiveOutput(TransformPipe):
 
-    __shortname__ = 'print'
+    def init(self, fields):
+        self.fields = fields
 
-    def aggregate(self, data, cluster, results=None):
-
-        for row in cluster.rows:
-            data[row].update(cluster.props)
-            output = [ str(data[row][k]) for k in self.fields ]
+    def apply(self, row):
+        if type(row) == list:
+            for r in row:
+                self.apply(r)
+        else:
+            output = [ str(row[k]) for k in self.fields ]
             print( '\t'.join( output ) )
 
-if __name__ == "__main__":
+    @property
+    def type(self):
+        return 'output'
 
-    def extract_dict(d, prefix):
-        nd = {}
-        for key in d:
-            if key.startswith(prefix):
-                nd[key[len(prefix):]] = d[key]
-        return nd
+################################################################################
 
-    parser = argparse.ArgumentParser(description='Hadoop streaming date clusterer.')
-    parser.add_argument('fields', action='store', default=None)
-    parser.add_argument('--out', dest='output_fields', action='store', default=None)
-    parser.add_argument('--patch', action='store', default=None)
-    parser.add_argument('--agg', action='store', default=None)
+class DateClusterer(TransformPipe):
 
-    args, _ = parser.parse_known_args()
-    if args.patch is not None:
-        exec(open(args.patch))
+    def init(self, fields=None):
+        self.groups_handled = {}
+        if isinstance(fields, str):
+            fields = fields.split(',')
+        assert len(fields) in (3,4)
+        grouped = len(fields) == 4
 
-    subparsers = parser.add_subparsers(help='sub-command help')
-    for clusterClass in AbstractClusterer.__subclasses__():
-        clusterClass.setup_parsers(subparsers)
+        if grouped:
+            self.grouped = True
+            self.id, self.group, self.start, self.end = fields
+        else:
+            self.grouped = False
+            self.id, self.start, self.end = fields
 
-    args = parser.parse_args()
+    def merge_clusters(self, clusters, *indices):
+        indices = sorted(indices)
+        i = indices[0]
+        dout = clusters[i]
+        for index in indices[:1:-1]:
+            dout.merge(clusters[index])
+            del clusters[index]
+        return clusters
 
-    input_fields = args.fields.split(',')
-    assert 'id_cluster' not in input_fields
+    def add_to_clusters(self, clusters, id, start, end, *rows):
+        indices = []
 
-    if args.output_fields is not None:
-        output_fields = args.output_fields.split(',')
-    else:
-        output_fields = ['id_cluster'] + input_fields
+        for i,date_cluster in enumerate(clusters):
+            if date_cluster.should_include(start, end):
+                indices.append(i)
 
-    if args.agg is None:
-        agg = PrintAggregator(fields=output_fields)
-    else:
-        for aggClass in AbstractAggregator.__subclasses__():
-            if aggClass.__shortname__ == args.agg:
-                agg = aggClass(fields=output_fields)
+        if len(indices) == 0:
+            clusters.append(DateCluster(id, start, end, *rows))
+            return clusters
 
-    csv_reader = csv.DictReader(sys.stdin, delimiter='\t', doublequote=False, fieldnames=input_fields)
+        if len(indices) > 0:
+            clusters = self.merge_clusters(clusters, *indices)
+        clusters[indices[0]].add(*rows)
 
-    for clusterClass in AbstractClusterer.__subclasses__():
-        if clusterClass.__shortname__ == args.which:
-            clusterer = clusterClass(**extract_dict(vars(args),clusterClass.__shortname__+'_'))
-            clusterer.cluster(csv_reader, agg=agg)
-            break
+        return clusters
+
+    def apply(self, data):
+        cur_group = None
+        cur_index = 0
+        cur_data = []
+        clusters = []
+        r = None
+        for row, entry in enumerate(data):
+            if self.grouped and entry[self.group] != cur_group:
+                if cur_group != None:
+                    for cluster in self.yield_clusters(cur_data, clusters, cur_group):
+                        yield cluster
+                    clusters = []
+                    cur_data = []
+                    cur_index = 0
+                cur_group = entry[self.group]
+            cur_data.append(entry)
+            self.add_to_clusters(clusters, entry[self.id], entry[self.start], entry[self.end], cur_index)
+            cur_index += 1
+        for cluster in self.yield_clusters(cur_data, clusters, cur_group):
+            yield cluster
+
+    def yield_clusters(self, data, clusters, group):
+        if group in self.groups_handled:
+            raise ValueError("Group already processed for %s." % group)
+        for cluster in clusters:
+            c_dicts = []
+            for row in cluster.rows:
+                c_dict = data[row]
+                c_dict.update(cluster.props)
+                c_dicts.append(c_dict)
+            yield c_dicts
+        self.groups_handled[group] = True
+
+    @property
+    def type(self):
+        return 'cluster'
+
+class DateCluster(object):
+
+    def __init__(self, id, date_start, date_end, *rows):
+        self.id = id
+        self.date_start = date_start
+        self.date_end = date_end
+        self.rows = list(rows)
+
+    def __repr__(self):
+        return "DateCluster(%s, %s, %d)" % (self.date_start, self.date_end, len(self.rows))
+
+    def merge(self, other):
+        self.date_start = min(self.date_start, other.date_start)
+        self.date_end = min(self.date_end, other.date_end)
+        self.rows.extend(other.rows)
+
+    def should_include(self, start, end):
+        return (start <= self.date_end) and (end >= self.date_start)
+
+    def add(self, *rows):
+        self.rows.extend(list(rows))
+
+    @property
+    def props(self):
+        return {'id_cluster': self.id, 'cluster_start': self.date_start, 'cluster_end': self.date_end}
+
+def debug(x):
+    print(x, file=sys.stderr)
+
+t = Transform()\
+    .input(HiveToDictInput, fields=['id','start','end','group'])\
+    .cluster(DateClusterer, fields=['id','group','start','end'])\
+    .output(DictToHiveOutput, fields=['id_cluster','id'])\
+    .output(debug)\
+    .apply(sys.stdin)
