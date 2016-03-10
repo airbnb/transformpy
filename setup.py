790cb510870b49076bad7eee09327fbc2ec3c7ce
Matthew Wardrop <matthew.wardrop@airbnb.com>
2016-03-10 10:55:52 -0800

    Make pip installable.

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup
+
+# Used to read README.md into long description
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+setup(
+    name = "transformpy",
+    version = "0.1",
+    author = "Matthew Wardrop",
+    author_email = "matthew.wardrop@airbnb.com",
+    description = ("A general purpose python ETL/pipeline utility library, for use especially with Hive Streaming."),
+    keywords = "pipeline transform hive",
+    packages=['transformpy', 'transformpy.pipes'],
+    long_description=read('README.md'),
+)
