783d09394519bea2e7143de124b6cecf4225ad00
Matthew Wardrop <matthew.wardrop@airbnb.com>
2017-06-11 15:48:10 -0700

    Add support for specifying Python Hive schema, and add support for detecting (and converting) null fields.

--- transformpy/pipes/hive.py
+++ transformpy/pipes/hive.py
@@ -8,10 +8,27 @@ __all__ = ['HiveToDictInput', 'DictToHiveOutput']
 class HiveToDictInput(SourcePipe):
 
     def init(self, fields):
-        self.fields = fields
+        self.fields = []
+        self.schema = {}
+
+        def wrap_conv(field, conv):
+            def wrapped(x):
+                try:
+                    return conv(x)
+                except Exception as e:
+                    raise RuntimeError("Failed to convert field `{}` with value '{}'. Error was: {}".format(field, x, str(e)))
+            return wrapped
+
+        for field in fields:
+            if isinstance(field, (list, tuple)):
+                self.fields.append(field[0])
+                self.schema[field[0]] = wrap_conv(field[0], field[1])
+            else:
+                self.fields.append(field)
 
     def apply(self, data):
-        return csv.DictReader(data, delimiter='\t', doublequote=False, fieldnames=self.fields)
+        for row in csv.DictReader(data, delimiter='\t', doublequote=False, fieldnames=self.fields):
+            yield {k: None if v == r'\N' else self.schema.get(k, lambda x: x)(v) for k, v in row.items()}
 
 
 class DictToHiveOutput(SinkPipe):
