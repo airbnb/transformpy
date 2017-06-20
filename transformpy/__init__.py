a7865b55dba533d7a32cacc54008684d690a221a
Matthew Wardrop <matthew.wardrop@airbnb.com>
2017-06-19 23:32:45 -0700

    Bump version to 0.3.1

--- transformpy/__init__.py
+++ transformpy/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 from .base import *
+from .utils.testing import TransformTester
 
 __author__ = 'Matthew Wardrop <matthew.wardrop@airbnb.com>'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
