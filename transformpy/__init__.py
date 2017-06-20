29b0025f08de211007a6766e884df3ba7620ea22
Matthew Wardrop <matthew.wardrop@airbnb.com>
2017-06-19 23:51:15 -0700

    Remove accidentally committed WIP code.

--- transformpy/__init__.py
+++ transformpy/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
 from .base import *
-from .utils.testing import TransformTester
 
 __author__ = 'Matthew Wardrop <matthew.wardrop@airbnb.com>'
 __version__ = '0.3.1'
